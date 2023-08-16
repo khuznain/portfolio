55c2ac78777cba6b93d1226adefd486fda449c8d
khalid <khalid.khan@five.me>
2023-08-16 22:56:13 +0500

    added Projects

---
+++ components/Projects.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+//
+import { projectsData } from "@/constant/data";
+import SectionHeading from "./SectionHeading";
+import Project from "./Project";
+
+export default function Projects() {
+  return (
+    <section className="scroll-mt-28 mb-28">
+      <SectionHeading>My Projects</SectionHeading>
+      {projectsData.map((project, index) => {
+        return (
+          <React.Fragment>
+            <Project key={index} {...project} />
+          </React.Fragment>
+        );
+      })}
+    </section>
+  );
+}
